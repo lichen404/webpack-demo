4736719c0f23eda754834fc58bfe64abf06e1ae6
lichen404 <f360family@gmail.com>
2019-12-22 11:39:22 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,3 @@ git add . &&
 git commit -m 'update' &&
 git push origin gh-pages
 
-yarn build &&
-git switch gh-pages &&
-rm -rf *.html *.js *.css *.png &&
-mv dist/* ./ &&
-rm -rf dist;
-git add . &&
-git commit -m 'update' &&
-git push origin gh-pages
-
